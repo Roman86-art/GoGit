d844b81eb9bf428ddca3846a83ba35cee0e6a0a6
Roman86-art <65312196+Roman86-art@users.noreply.github.com>
2020-06-24 16:23:25 -0500

    Update MarketAREHot.java

--- src/test/java/Homework7/MarketAREHot.java
+++ src/test/java/Homework7/MarketAREHot.java
@@ -3,7 +3,7 @@ package Homework7;
 public class MarketAREHot {
     public static void main(String[] args) {
         System.out.println("tak tak tak ");
-        System.out.println(asjhdjkashdjashjdhaskjdjaskfhashfdshjfh);
+        System.out.println(asjfh);
 
     }
 }
