0d19e903fe88bcbc80812ada7644693800e6529c
Silond <silond@gmail.com>
2020-06-24 16:11:05 -0500

    hello to you

--- src/test/java/Homework7/newThing.java
+++ src/test/java/Homework7/newThing.java
@@ -1,4 +1,10 @@
 package Homework7;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 public class newThing {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
